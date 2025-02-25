7b5e74ffb62837cca3273897b22de258c08ebb91
tanishka-pandey <pandeytanishka252@gmail.com>
2025-02-25 20:10:17 +0530

    Updated version

--- project2.py
+++ project2.py
@@ -12,12 +12,6 @@ class Card:
     def __str__ (self):
         return  f"{self.rank}  of  {self.suit} "
 
-c1 = Card('Spades','Four') 
-c1.suit
-print(c1)
-
-
-
 class Deck:
     def __init__(self):
         self.all_cards= []
@@ -29,17 +23,6 @@ class Deck:
         return random.shuffle(self.all_cards)
     def one_card(self):
         return self.all_cards.pop()
-
-
-
-
-new_deck = Deck()
-new_deck.shuffle()
-new_deck .all_cards[0]
-print(new_deck .all_cards[0])
-#for card in new_deck.all_cards :
-    #print(card)
-
 
 class  Player:
     def __init__(self,name):
@@ -55,5 +38,26 @@ class  Player:
     def __str__(self):
         return f'Player {self.name} has {(len(self.all_cards))} cards'        
 
-new_player= Player("neha")
-print(new_player)
+player_one= Player("One")
+player_two =Player("Two")
+new_deck = Deck()
+new_deck.shuffle()
+for x in range(26):
+    player_one.add(new_deck.one_card())
+    player_two.add(new_deck.one_card())
+game_on= True
+round_num= 0 
+while game_on:
+    round_num +=1
+    print(f" Round : {round_num}")
+    if len(player_one.all_cards()) ==0:
+        print("Player one is out of cards!!, Player Two wins")
+        break
+    if len(player_two.all_cards()) ==0:
+        print("Player Two is out of cards!!, Player Owo wins")
+        break
+    player_one_current =[]
+    player_one_current.append(player_one.remove())
+    player_two_current =[]
+    player_two_current.append(player_two.remove())
+    
